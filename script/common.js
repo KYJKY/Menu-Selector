7ea12c3208569e4b93cd83eadc67683b2719d9c2
KYJKY <kyj_3052@naver.com>
2022-04-23 00:00:59 +0900

    [add] 돌림판 추가

---
+++ script/common.js
@@ -0,0 +1,10 @@
+function gnb() {
+    const home = document.getElementById('home');
+    const menuList = document.getElementById('menuList');
+    home.addEventListener('click', () => {
+        location.href='index.html'
+    });
+    menuList.addEventListener('click', () => {
+        
+    });
+}
